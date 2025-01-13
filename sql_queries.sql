0bc29820d89b4703e4a20774b6aa2a0fe492f171
akashedara378 <136588998+akashedara378@users.noreply.github.com>
2025-01-13 15:22:09 +0530

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -194,7 +194,11 @@ order by state DESC;
 select * from customers, orders;
 
 -- inner
-select * from customers c join orders o on c.customer_id = o.customer_id;
+select * from customers c
+join
+orders o
+on c.customer_id = o.customer_id;
+
 select * from customers c INNER JOIN orders o on c.id = o.id;
 
 -- left
