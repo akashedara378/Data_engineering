3fb8219012e6ef5a1d5b87c05f92ef5bc3decd0b
akashedara378 <136588998+akashedara378@users.noreply.github.com>
2025-03-19 09:44:28 +0530

    Update sort.py

--- python/DSA/array/sort.py
+++ python/DSA/array/sort.py
@@ -10,3 +10,18 @@ for i in range(n):
             
 
 print(data)
+
+
+# selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:  
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]  # Swap
+
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print(arr)  # [11, 12, 22, 25, 64]
