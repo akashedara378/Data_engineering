b85386ee33b024e184e786002383ea07653fdf73
akashedara378 <136588998+akashedara378@users.noreply.github.com>
2025-04-16 16:24:20 +0530

    Update databricks.py

--- databricks.py
+++ databricks.py
@@ -1,3 +1,15 @@
+#cluster details
+1 Driver node: Standard_E8s_v3 (8 vCPUs, 64 GB RAM)
+10 Executor (worker) nodes: Standard_E8s_v3 (each with 8 vCPUs, 64 GB RAM)
+Total vCPUs: 80
+Default parallelism: ~80
+Shuffle partitions: Set to 800 (10x parallelism for better performance)**
+You're telling Spark to split shuffled data into 800 partitions.
+
+1,000,000 MB / 128 MB ≈ ~7800 partitions
+ 6000-8000 ideal and 300-400 mb of file.
+
+
 # Define parameters in Databricks
 dbutils.widgets.text("input_file", "")
 dbutils.widgets.text("output_path", "")
